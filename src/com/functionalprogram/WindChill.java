e28df084889fe070e4c2dc5ea7ec7d21b780797d
Kajakwankhede <Kajalwankhede@example.com>
2022-10-11 12:13:57 +0530

    ADDED Windchill Prgram

--- src/com/functionalprogram/WindChill.java
+++ src/com/functionalprogram/WindChill.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 
 public class WindChill {
     public static void main(String[] args) {
-        System.out.println("Enter value of temperature in fahrenheit  : ");
+        System.out.println("Enter value of temperature  : ");
         Scanner sc = new Scanner(System.in);
-        double temperature = sc.nextDouble();
+        double temp = sc.nextDouble();
         System.out.println("Enter wind speed in miles per hour : ");
-        double velocity = sc.nextDouble();
-        if ((temperature < 50) && ((velocity > 120) || (velocity < 3))) {
-            double speed = Math.pow(velocity, 0.16);
-            double product = ((0.4275 * temperature) - 35.75);
-            double windChill = 35.74 + (0.6215 * temperature) + (product * speed);
+        double WindSpeed = sc.nextDouble();
+        if ((temp < 50) && ((WindSpeed > 120) || (WindSpeed < 3))) {
+            double speed = Math.pow(WindSpeed, 0.16);
+            double product = ((0.4275 * temp) - 35.75);
+            double windChill = 35.74 + (0.6215 * temp) + (product * speed);
             System.out.println("Wind chill : ");
             System.out.println("RESULT = " + windChill);
         } else
